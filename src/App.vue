ebdf36f2169f2f07a8fc1f25ffd71ee1d33c4f82
kaczor6418 <k.kaczynski97@gmail.com>
2021-03-06 12:59:24 +0100

    style(project): set default font and font color for whole project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <HelloWorld msg="Rick and morty" />
+  <icon :icon-id="iconId" />
+  <v-label value="Dupa"></v-label>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Icon from "./components/atoms/icon/Icon.vue";
+import { IconId } from "@/common/IconsDefinitions/IconId";
+import VLabel from "@/components/atoms/label/VLabel.vue";
 
-export default defineComponent({
-  name: "App",
+export default {
   components: {
-    HelloWorld
+    VLabel,
+    Icon
+  },
+  setup() {
+    const iconId = IconId.STAR;
+    return {
+      iconId: iconId
+    };
   }
-});
+};
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Poppins", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #a9b1bd;
   margin-top: 60px;
 }
 </style>
