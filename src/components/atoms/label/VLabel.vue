d8d7a717e85fa393d2f9afc23e96a292f6dfd802
kaczor6418 <k.kaczynski97@gmail.com>
2021-03-06 12:58:59 +0100

    feat(label): create label component

---
+++ src/components/atoms/label/VLabel.vue
@@ -0,0 +1,17 @@
+<template>
+  <label>{{ value }}</label>
+</template>
+
+<script lang="ts">
+export default {
+  name: "VLabel",
+  props: {
+    value: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
