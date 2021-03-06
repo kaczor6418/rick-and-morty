3aa0ddad4b999962fabb9e0c557327ed687950bd
kaczor6418 <k.kaczynski97@gmail.com>
2021-03-06 17:38:53 +0100

    feat(input): add input component

---
+++ src/components/atoms/input/VInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <input
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
+
+<script lang="ts">
+export default {
+  name: "VInput",
+  props: {
+    modelValue: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+input {
+  color: #a9b1bd;
+  border: 1px solid #a9b1bd;
+}
+</style>
