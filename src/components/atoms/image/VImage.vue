41c707d157299e2f9bee4c07f8e129dff2486a44
kaczor6418 <k.kaczynski97@gmail.com>
2021-03-07 20:42:00 +0100

    feat(image-component): create image component

---
+++ src/components/atoms/image/VImage.vue
@@ -0,0 +1,27 @@
+<template>
+  <img :style="{ maxWidth: width }" :src="url" :alt="description" />
+</template>
+
+<script>
+export default {
+  name: "VImage",
+  props: {
+    url: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    width: {
+      type: String,
+      default: "43px"
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
