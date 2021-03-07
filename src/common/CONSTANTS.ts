28ab1af590b06e6067660db14f58148a6a22cb65
kaczor6418 <k.kaczynski97@gmail.com>
2021-03-07 19:29:13 +0100

    chore(project): add and setup vue-apollo in project

---
+++ src/common/CONSTANTS.ts
@@ -0,0 +1,3 @@
+export namespace CONSTANTS {
+  export const RICK_AND_MORTY_API_URI = "https://rickandmortyapi.com/graphql";
+}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,27 @@
-import { createApp } from "vue";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from "@apollo/client/core";
+import { createApp, provide, h } from "vue";
 import App from "./App.vue";
+import { CONSTANTS } from "@/common/CONSTANTS";
+import { DefaultApolloClient } from "@vue/apollo-composable";
 
-createApp(App).mount("#app");
+const httpLink = createHttpLink({
+  uri: CONSTANTS.RICK_AND_MORTY_API_URI
+});
+const cache = new InMemoryCache();
+const defaultApolloClient = new ApolloClient({
+  link: httpLink,
+  cache
+});
+
+createApp({
+  setup() {
+    provide(DefaultApolloClient, defaultApolloClient);
+  },
+  render() {
+    return h(App);
+  }
+}).mount("#app");
